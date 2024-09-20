21d2f6c1817d293e8238a09eedeb2911d098f666
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2024-09-20 20:33:00 +0900

    Add slf4j-simple deps to examples

--- examples/register_mnist.java
+++ examples/register_mnist.java
@@ -3,6 +3,7 @@
 //JAVA 17+
 //REPOS mavencentral,jitpack=https://jitpack.io
 //DEPS com.github.tadayosi:torchserve-client-java:main-SNAPSHOT
+//DEPS org.slf4j:slf4j-simple:1.7.36
 
 import com.github.tadayosi.torchserve.client.TorchServeClient;
 import com.github.tadayosi.torchserve.client.model.ApiException;
