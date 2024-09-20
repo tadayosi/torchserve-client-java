1ecc8e1b0cf1cbe76a94b9d8db8142c7341339db
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2024-09-20 16:28:49 +0900

    Add example token_auth.java

---
+++ examples/token_auth.java
@@ -0,0 +1,29 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+//JAVA 17+
+//REPOS mavencentral,jitpack=https://jitpack.io
+//DEPS com.github.tadayosi:torchserve-client-java:main-SNAPSHOT
+
+import com.github.tadayosi.torchserve.client.TorchServeClient;
+import com.github.tadayosi.torchserve.client.model.ApiException;
+
+public class token_auth {
+
+    /**
+     * You can find the auth token for the inference API by looking at the
+     * `key_file.json` in the current directory of the TorchServe server.
+     */
+    private static String INFERENCE_AUTH_TOKEN = "<inference-key>";
+
+    public static void main(String... args) throws Exception {
+        try {
+            var client = TorchServeClient.builder()
+                    .inferenceKey(INFERENCE_AUTH_TOKEN)
+                    .build();
+            var response = client.inference().ping();
+            System.out.println(response);
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
